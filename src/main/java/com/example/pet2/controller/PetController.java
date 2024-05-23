ea53321feb3841962d12a8fefe3774714f7046dd
itf <itf@host.docker.internal>
2024-05-23 15:33:26 +0900

    GET(参照)を実装

--- src/main/java/com/example/pet2/controller/PetController.java
+++ src/main/java/com/example/pet2/controller/PetController.java
@@ -1,13 +1,22 @@
 package com.example.pet2.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.pet2.model.Pet;
+
 @RestController
+@RequestMapping("/pet")
 public class PetController {
 
-	@GetMapping("/pet")
-	public String index() {
-		return "アクセス成功です";
+	@GetMapping
+	public List<Pet> getAllPets() {
+		List<Pet> allPets = Arrays.asList(
+				new Pet(null, null, "10001", null, null, null));
+		return allPets;
 	}
 }
