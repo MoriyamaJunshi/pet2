8335a8be95a4b300b2eeb164ce2a977694adac07
itf <itf@host.docker.internal>
2024-05-23 16:16:58 +0900

    GET(参照)を実装2

--- src/main/java/com/example/pet2/controller/PetController.java
+++ src/main/java/com/example/pet2/controller/PetController.java
@@ -1,22 +1,30 @@
 package com.example.pet2.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.pet2.model.Pet;
+import com.example.pet2.service.PetService;
 
 @RestController
 @RequestMapping("/pet")
 public class PetController {
+	
+	@Autowired
+	private PetService petService;
 
 	@GetMapping
 	public List<Pet> getAllPets() {
-		List<Pet> allPets = Arrays.asList(
-				new Pet(null, null, "10001", null, null, null));
-		return allPets;
+		return petService.getAllPets();
+	}
+	
+	@GetMapping("/{petId}")
+	public Pet getPet(@PathVariable("petId") String petId) {
+		return petService.getPetId(petId);
 	}
 }
---
+++ src/main/java/com/example/pet2/service/PetService.java
@@ -0,0 +1,28 @@
+package com.example.pet2.service;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.pet2.model.Pet;
+
+@Service
+public class PetService {
+
+	private List<Pet> allPets = Arrays.asList(
+			new Pet(null, null, "10001", null, null, null));
+	
+	public List<Pet> getAllPets(){
+		return allPets;
+	}
+	
+	public Pet getPetId(String PetId) {
+		for(int i=0; i<allPets.size(); i++) {
+			if(allPets.get(i).getId().equals(PetId)) {
+				return (Pet)allPets.get(i);
+			}
+		}			
+		return null;
+	}
+}
